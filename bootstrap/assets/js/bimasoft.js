a3bba8c7a4987e57d9e9ce1c2a2bc36e09c447e8
Wasis Haryo Sasoko <yokowasis@gmail.com>
2017-09-12 03:35:22 +0800

    Product & Services Finish

---
+++ bootstrap/assets/js/bimasoft.js
@@ -0,0 +1,41 @@
+function extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+
+function extractRootDomain(url) {
+    var domain = extractHostname(url),
+        splitArr = domain.split('.'),
+        arrLen = splitArr.length;
+
+    //extracting the root domain here
+    if (arrLen > 2) {
+        domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+    }
+    return domain;
+}
+
+jQuery(document).ready(function($) {
+
+	$('.dtt').DataTable({
+		"initComplete": function(settings, json) {
+		    $('.dataTables_filter input[type=search]').prop({
+		    	placeholder: 'Enter search term...'
+		    });
+		  }
+	});
+});
