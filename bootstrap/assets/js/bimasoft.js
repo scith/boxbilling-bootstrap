f1effbdcdf2a8042a0909f5894dcaf79130d4fca
YokoWasis <yokowasis@gmail.com>
2017-09-24 14:40:18 +0800

    Auto Login CWP

--- bootstrap/assets/js/bimasoft.js
+++ bootstrap/assets/js/bimasoft.js
@@ -1,3 +1,13 @@
+function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
 function extractHostname(url) {
     var hostname;
     //find & remove protocol (http, ftp, etc.) and get hostname
@@ -48,6 +58,47 @@ jQuery(document).ready(function($) {
         window.location = url;
     });
 
+    $('#optional_manage button[value="Change password"]').click(function(event) {
+        $('#loaderwrapper').show();
+        var myVar = setInterval(function(){
+            if (($(".bootbox").data('bs.modal') || {}).isShown) {
+                clearInterval(myVar);
+                $('#loaderwrapper').hide();         
+            }
+        }, 300);
+    });
+
+    $('#optional_manage a').each(function(index, el) {
+        href = $(this).prop('href');
+        if (href.indexOf('2031') !== -1) {
+            $(this).before(`
+                    <form id="cwpanel_login" target="cwpanel_window" action="`+ href +`login/index.php" method="post">
+                        <input type="hidden" name="username" value="">
+                        <input type="hidden" name="password" value="">
+                        <input type="hidden" name="commit" value="login">
+                        <input type=button class='btn btn-primary' id="cwpanel_login_btn" value="Login to CWPanel">
+                    </form>`);
+            $(this).remove();
+            $('#cwpanel_login_btn').click(function(event) {
+                window.open(href + 'login/index2.php','cwpanel_window');
+                randompass = generatePassword();
+                username = $('#tab-details').find('tr').eq(3).find('td').eq(1).text();
+                $('#optional_manage input[type="password"][name="password"]').val(randompass);
+                $('#optional_manage input[type="password"][name="password_confirm"]').val(randompass);
+                $('#optional_manage button[type=submit][value="Change password"]').click();
+                var myVar = setInterval(function(){
+                    if (($(".bootbox").data('bs.modal') || {}).isShown) {
+                        clearInterval(myVar);
+                        $('#loaderwrapper').hide();
+                        $(".bootbox.modal").modal('hide');
+                        $('#cwpanel_login input[name="username"]').val(username);
+                        $('#cwpanel_login input[name="password"]').val(randompass);
+                        $('#cwpanel_login').submit();
+                    }
+                }, 300);
+            });
+        }
+    });
 
 });
 
